9474f381e0f2a9b961c8a21fc4a81a2ece3cfd44
shxg66 <shxg66@gmail.com>
2023-04-13 18:30:17 -0400

    Initial Commit

---
+++ Demo App/Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_AppApp.swift
+//  Demo App
+//
+//  Created by Xuguang Shao on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
